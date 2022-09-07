77a0c015ae9405d683d20ff2c7055832cca8ddd9
James Baker <34027007+LSOffice@users.noreply.github.com>
2022-09-07 11:30:13 +0800

    Add files via upload

---
+++ ISLE-Scraper/main.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://isle.island.edu.hk/login/index.php'
+values = {'username': open("login.txt", "r").readlines()[0].replace("\n", ""),
+          'password': open("login.txt", "r").readlines()[1].replace("\n", "")}
+
+classes = []
+with requests.Session() as s:
+    r = s.post(url, data=values)
+    #print(r.text)
+    index_page = s.get("https://isle.island.edu.hk/")
+    soup = BeautifulSoup(index_page.text, 'html.parser')
+    mydivs = soup.find_all("div", {"class": "greeting"})
+    try:
+        tdTags = mydivs[0].find_all("div")
+    except IndexError:
+        raise KeyError("Invalid username/password")
+    correctdiv = tdTags[0]
+    txt = correctdiv.text
+    
+    today = correctdiv.text[:txt.find("DawnBreakfast")]
+    today_table = correctdiv.find_all("table")[0]
+    tr = today_table.find_all("tr")
+    for a in tr[1]:
+        classes.append({
+            "class": a.text[:7],
+            "room": a.text[10:]
+        })
+    
+    
+    index_page = s.get("https://isle.island.edu.hk/local/mis/reports/unitview.php")
+    soup = BeautifulSoup(index_page.text, 'html.parser')
+    spans = soup.find_all("span", {"class": "title"})
+    yeargroup = ""
+    for a in spans:
+        if "Yr" in a.text:
+            yeargroup = a.text.replace("Yr ", "")
+
+    if int(yeargroup) == 11:
+        mydivs = soup.find_all("div", {"class": "row-fluid"})
+        tablesneeded = []
+        for a in mydivs[1]:
+            table = a.find_all("table")
+            tablesneeded.append(table[0])
+        
+        tr1 = tablesneeded[0].find_all("tr")
+        tr1.pop(0)
+        
+        for tr1a in tr1:
+            tr1barray = []
+            for tr1b in tr1a:
+                tr1barray.append(tr1b.text)
+            #try:
+            #    print(f"{tr1barray[0]}:\nYellis predicted: {tr1barray[1]}\nActual Grade: {tr1barray[3]}\n")
+            #except IndexError:
+            #    print(f"{tr1barray[0]}:\nYellis predicted: {tr1barray[1]}\nActual Grade: Missing\n")
+            
+        #table = soup.find('table', attrs={'class':' span6'})
+        #print(table)
+    else:
+        print("Grades could not be loaded because format has not been initiated. Please contact the administrator of this program")
+
+classccount = 1
+for classc in classes:
+    print(f"Period {classccount}: {classc['class']} in room {classc['room']}")
+    classccount += 1
