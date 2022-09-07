8c3bfd4547220ff02c0f452048a2c6ebbbcd36a5
James Baker <34027007+LSOffice@users.noreply.github.com>
2022-09-07 13:32:48 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+ECHO This windows script has not been configured to work. Please contact an administrator before continuing 
+ECHO Error: Bad Request
+
+PAUSE
---
+++ start.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Downloads/ISLE-Scraper-main/ISLE-Scraper
+pip install -r requirements.txt
+python main.py
