be4d3a7ba8c7ca3dd236c384d2fa48aaab8bdc8f
James Baker <34027007+LSOffice@users.noreply.github.com>
2022-09-20 10:20:18 +0800

    Add files via upload

--- ISLE-Scraper/main.py
+++ ISLE-Scraper/main.py
@@ -1,9 +1,26 @@
-import requests
+import os, json, datetime
+
+now = datetime.datetime.now()
+timenow = (int(now.strftime("%H")), int(now.strftime("%M")), int(now.strftime("%S")))
+print(timenow)
+timetable = [[(8, 20, 0), (9, 0, 0)], [(9, 0, 1), (9, 40, 0),], [(9, 45, 0), (10, 25, 0)], [(10, 25, 1), (11, 0, 0)], [(11, 25, 0), (12, 5, 0)], [(12, 5, 1), (12, 45, 0)], [(13, 40, 0), (14, 20, 0)], [(14, 20, 1), (15, 0, 0)]]
+
+a = json.load(open("settings.json", "r+"))
+if not a['reqinstalled']:
+    os.system("pip3 install -r requirements.txt")
+    a['reqinstalled'] = True
+    json.dump(a, open("settings.json", "w"), indent=4)
+else:
+    print("pip3 installation skipped as requirements are installed")
+
+import requests, sys
 from bs4 import BeautifulSoup
 
 url = 'https://isle.island.edu.hk/login/index.php'
 values = {'username': open("login.txt", "r").readlines()[0].replace("\n", ""),
           'password': open("login.txt", "r").readlines()[1].replace("\n", "")}
+args = sys.argv
+#args.pop(0)
 
 classes = []
 with requests.Session() as s:
@@ -57,24 +74,91 @@ with requests.Session() as s:
                 print(f"{tr1barray[0]}:\nYellis predicted: {tr1barray[1]}\nActual Grade: Missing\n")
     else:
         raise KeyError("Grades could not be loaded because format has not been initiated. Please contact the administrator of this program")
+    
+if "Friday" in today:
+    del classes[-2:]
 
 from tkinter import *
-window=Tk()
-window.title('Timetable')
-window.geometry("250x350")
-classccount = 1
-lbl=Label(window, text=f"{today}", fg='black', font=("Helvetica", 16))
-lbl.place(x=5, y=5)
-cury = 30
-for classc in classes:
-    lbl=Label(window, text=f"Period {classccount}: {classc['class']} in room {classc['room']}", fg='red', font=("Helvetica", 16))
-    lbl.place(x=5, y=cury)
-    cury+=30
-    classccount += 1
-    
-def exitProgram():
-   exit()
+from tkinter import colorchooser
+if "--nogui" in args:
+    if sys.platform == "darwin":
+        try:
+            file = open("timetable.txt", "x")
+        except FileExistsError:
+            os.remove("timetable.txt")
+            file = open("timetable.txt", "x")
+            
+        file.write("Timetable\n")
+        classccount = 1
+        for classc in classes:
+            file.write(f"Period {classccount}: {classc['class']} in room {classc['room']} ({timetable[classccount-1][0]}-{timetable[classccount-1][1]})\n")
+            classccount += 1
+            
+        import subprocess
+        subprocess.call(['open', "timetable.txt"])
+    else:
+        raise IndexError("No GUI mode not supported for windows. Please remove the --nogui tag when running this program")
+else:
+    a = json.load(open("settings.json", "r+"))
+    window=Tk()
+    window.title('Timetable')
+    window.geometry("250x350")
+    window.configure(background=a['backgroundcolour'])
+    classccount = 1
+    lbl=Label(window, text=f"{today}", fg=a['titlecolour'], font=("Helvetica", 16))
+    lbl.place(x=5, y=5)
+    cury = 30
+    for classc in classes:
+        lbl=Label(window, text=f"Period {classccount}: {classc['class']} in room {classc['room']}", fg=a['textcolour'], font=("Helvetica", 16))
+        lbl.place(x=5, y=cury)
+        cury+=30
+        classccount += 1
+        
+    def exitProgram():
+        exit()
 
-btn = Button(window, text="Exit", command=exitProgram)
-btn.place(x=5, y=270)
-window.mainloop()
+    btn = Button(window, text="Exit", command=exitProgram)
+    btn.place(x=5, y=270)
+    
+    def choosetextcolour():
+        color_code = colorchooser.askcolor(title ="Choose text colour")
+        a['textcolour'] = color_code[1]
+        json.dump(a, open("settings.json", "w"), indent=4)
+        
+    def choosebgcolour():
+        color_code = colorchooser.askcolor(title ="Choose background colour")
+        a['backgroundcolour'] = color_code[1]
+        json.dump(a, open("settings.json", "w"), indent=4)
+        
+    def choosetitlecolour():
+        color_code = colorchooser.askcolor(title ="Choose title colour")
+        a['titlecolour'] = color_code[1]
+        json.dump(a, open("settings.json", "w"), indent=4)
+    
+    def okbutton():
+        exit("Program closed")
+    
+    def applychanges():
+        new=Toplevel(window)
+        new.geometry("400x50")
+        new.title("Reload your app to confirm the changes")
+        btn = Button(new, text="Ok", command=okbutton)
+        btn.place(x=5, y=5)
+    
+    def settings():
+        new= Toplevel(window)
+        new.geometry("200x150")
+        new.title("Settings")
+        btn = Button(new, text="Text Colour Pick", command=choosetextcolour)
+        btn.place(x=5, y=5)
+        btn = Button(new, text="Background Colour Pick", command=choosebgcolour)
+        btn.place(x=5, y=35)
+        btn = Button(new, text="Title Colour Pick", command=choosetitlecolour)
+        btn.place(x=5, y=65)
+        btn = Button(new, text="Apply Changes", command=applychanges)
+        btn.place(x=5, y=95)
+        
+        
+    btn = Button(window, text="Settings", command=settings)
+    btn.place(x=5, y=290)
+    window.mainloop()
